c2ee69a47e30efde0ee59025b28ed18a0606753e
SaRu-N <saruniroula73@gmail.com>
2022-05-22 13:36:12 +0545

    function explained

---
+++ functions.py
@@ -0,0 +1,9 @@
+def FunctionOne():
+    name=input("what is your name?")
+    print("Hello! "+name)
+def FunctionTwo(a, b):
+    print("sum= "+str(a+b))
+
+# calling function
+FunctionOne()
+FunctionTwo(4,7)
