b276d079e5c9b055a6a8b9f73086898954d9413f
SaRu-N <saruniroula73@gmail.com>
2022-05-22 13:24:18 +0545

    tuples in python

---
+++ tuples.py
@@ -0,0 +1,10 @@
+# tuples are data structures
+coordinates = (4, 6)
+
+# tuples are immutable cannot chagne,delete or add
+# coordinates[1]=10 this throws error
+print(coordinates[0])
+coordinates1=[(4,5),(6,7),(8,9)] #list of tuples
+print(coordinates1[2])
+coordinates1[2]=(50,89)
+print(coordinates1[2])
