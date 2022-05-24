3a7975538d2df3f769440fdaaff4db0eaf065441
SaRu-N <saruniroula73@gmail.com>
2022-05-24 12:37:19 +0545

    comparision done

--- ifstatements.py
+++ ifstatements.py
@@ -1,19 +1,19 @@
 
-# is_male =True
-# is_tall =False
-# # if is_male:
-# #     print("You are a male")
-# # else:
-# #     print("May be you are Female")
-# # 
-# if is_male and is_tall:
-#     print("You are a tall male")
-# elif is_male and not(is_tall):
-#     print("You are a short male")
-# elif is_tall and not(is_male):
-#     print("You are tall but not man")
+is_male =True
+is_tall =False
+# if is_male:
+#     print("You are a male")
 # else:
-#     print("May be you are Female or not tall")
+#     print("May be you are Female")
+# 
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male")
+elif is_tall and not(is_male):
+    print("You are tall but not man")
+else:
+    print("May be you are Female or not tall")
 
 def max_num(num1,num2,num3):
     if(num1>=num2 and num1>=num3):
