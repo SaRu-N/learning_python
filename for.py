59a8cb51f0285d8699f1e644baa8a3f08517ee03
SaRu-N <saruniroula73@gmail.com>
2022-05-24 13:18:26 +0545

    exponent function

--- for.py
+++ for.py
@@ -1,16 +1,23 @@
-for letter in "Learning Python":
-    print(letter)
-days =["Sunday", "Monday","Tuesday"]
-for day in days:
-    print(day)
-for index in range(2):
-    print(index)
-for item in range(2,5):
-    print(item)
-for index in range(len(days)):
-    print(days[index])
-for index in range(5):
-    if index==0:
-        print("First")
-    else:
-        print("not first")
+# for letter in "Learning Python":
+#     print(letter)
+# days =["Sunday", "Monday","Tuesday"]
+# for day in days:
+#     print(day)
+# for index in range(2):
+#     print(index)
+# for item in range(2,5):
+#     print(item)
+# for index in range(len(days)):
+#     print(days[index])
+# for index in range(5):
+#     if index==0:
+#         print("First")
+#     else:
+#         print("not first")
+
+def raise_to_power(base_num,power_num):
+    result=1
+    for index in range(power_num):
+        result=base_num*base_num
+    return result
+print("The result is "+str(raise_to_power(8,8)))
