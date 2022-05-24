6eaf31eca01905440b5f4433987349d0c6233c45
SaRu-N <saruniroula73@gmail.com>
2022-05-24 12:35:42 +0545

    dictonary explained

---
+++ dictonaries.py
@@ -0,0 +1,21 @@
+
+MonthConversions ={
+    1:"January",
+    2:"February",
+    3:"March",
+    4:"April",
+    5:"May",
+    6:"June",
+    7:"July",
+    8:"August",
+    9:"September",
+    10:"October",
+    11:"November",
+    12:"Decemober"
+    }
+print(MonthConversions[2])
+ #we can set default value  using get
+print(MonthConversions.get(12))
+print(MonthConversions.get(14,"there are only 12 months"))
+
+
