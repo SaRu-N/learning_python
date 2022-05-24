6f4b8d0be4041fad0212c13bd9801c735d8bf8cf
SaRu-N <saruniroula73@gmail.com>
2022-05-24 13:12:27 +0545

    for loop explained

---
+++ for.py
@@ -0,0 +1,16 @@
+for letter in "Learning Python":
+    print(letter)
+days =["Sunday", "Monday","Tuesday"]
+for day in days:
+    print(day)
+for index in range(2):
+    print(index)
+for item in range(2,5):
+    print(item)
+for index in range(len(days)):
+    print(days[index])
+for index in range(5):
+    if index==0:
+        print("First")
+    else:
+        print("not first")
