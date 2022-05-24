1a99bb3b318491018ec8acb328a28ef79216eabc
SaRu-N <saruniroula73@gmail.com>
2022-05-24 18:57:11 +0545

    playing quiz game

--- classes.py
+++ classes.py
@@ -7,4 +7,8 @@ class Student:
         self.major =major
         self.gpa=gpa
         self.is_on_probation=is_on_probation
+class Question:
+    def __init__(self,prompt,answer):
+        self.prompt=prompt
+        self.answer=answer
 
