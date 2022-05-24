bd30bf2ea7b2cc4e41416a18535741c6f52ad0cd
SaRu-N <saruniroula73@gmail.com>
2022-05-24 13:34:26 +0545

    translating  phrse

---
+++ translator.py
@@ -0,0 +1,14 @@
+# my own language
+# vowles=V
+def translate(phrase):
+    translation=""
+    for letter in phrase:
+        if letter.upper() in "AEIOU":
+            if letter.isupper():
+             translation=translation+"V"
+            else :
+              translation=translation+"v"
+        else:
+            translation=translation+letter
+    return translation
+print(translate(input("Enter a phrase:")))
