588d856064a7d4d61109142ec18acd96ed4de5d5
SaRu-N <saruniroula73@gmail.com>
2022-05-24 18:38:22 +0545

    Class and Object

--- basic.py
+++ basic.py
@@ -1,18 +1,24 @@
-print("Hello Python world")
-#  Python variables
-# strings
-user_name = "Alice"
-user_age="20"
-print("Hello there my name is "+user_name+".")
-print("I am "+user_age+ " years old.")
-user_name="John"
-user_age="25"
-print("Hello there my name is "+user_name+".")
-print("I am "+user_age+ " years old.")
-# numbers
-user_phone=9862700000
-user_salary=200000.00
+# print("Hello Python world")
+# #  Python variables
+# # strings
+# user_name = "Alice"
+# user_age="20"
+# print("Hello there my name is "+user_name+".")
+# print("I am "+user_age+ " years old.")
+# user_name="John"
+# user_age="25"
+# print("Hello there my name is "+user_name+".")
+# print("I am "+user_age+ " years old.")
+# # numbers
+# user_phone=9862700000
+# user_salary=200000.00
 
-# boolean
-isMale=True
-isFemale=False
+# # boolean
+# isMale=True
+# isFemale=False
+
+# #object of class
+from classes import Student
+student1=Student("John Abraham","Science",3.5,False)
+
+print(student1.gpa)
---
+++ object.py
@@ -0,0 +1,7 @@
+# #object of class
+from classes import Student
+student1= Student("John ","Science",3.5,False)
+student2= Student("Alice","Business",3.1,True)
+
+print(student1.name)
+print(student2.major)
