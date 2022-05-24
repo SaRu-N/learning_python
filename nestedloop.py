2d3558006a6a42960b0d725913b812bd949b4721
SaRu-N <saruniroula73@gmail.com>
2022-05-24 13:24:28 +0545

    nested list and loop

---
+++ nestedloop.py
@@ -0,0 +1,13 @@
+number_grid=[
+    [1,3,5,7,9],
+    [2,4,6,8],
+    [2,3,5,7],
+    [0]
+]
+# print("Even Numbers list "+str(number_grid[2]))
+# print(number_grid[1][3])
+
+for row in number_grid:
+    # print(row)
+    for col in row:
+        print(col)
